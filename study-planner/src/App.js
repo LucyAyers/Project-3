96d5763cda0ba737b8ed13b87b5259641c95bd77
LucyAyers <layers01@villanova.edu>
2026-04-21 15:33:47 -0400

    work on app.js

--- study-planner/src/App.js
+++ study-planner/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AssignmentPlanner from './AssignmentPlanner';
 
-function App() {
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="AssignmentPlanner"
+          component={AssignmentPlanner}
+          options={{ title: 'Assignment Planner' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-export default App;
